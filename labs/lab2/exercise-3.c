08cc9665e0dc30cc2dba73677be02fed4d5ec553
Harry Jane <harry.xinzyue@outlook.com>
2025-10-28 16:39:51 +0800

    finish all exercise 3 for lab 2

--- labs/lab2/exercise-3.c
+++ labs/lab2/exercise-3.c
@@ -1,19 +1,61 @@
+// #include <stdio.h>
+
+// int main(){
+//     int sum = 0;
+//     int num;
+//     double average;
+//     printf("Please enter 10 numbers:\n");
+//     for (int i = 1; i <= 10; i++){
+//         printf("Number #%d>> ", i);
+//         scanf("%d", &num);
+//         sum += num;
+//     }
+
+//     average = sum / 10.0;
+//     printf("The sum of 10 numbers is %d\n", sum);
+//     printf("The average is %.2lf\n", average);
+//     return 0;
+// }
+
+
+// #include <stdio.h>
+
+// int main(){
+//     int sum = 0;
+//     int num;
+//     double average;
+//     int count = 0;
+
+//     printf("Please enter 10 numbers\n");
+//     while (count < 10){
+//         printf("Number #%d>> ", count + 1);
+//         scanf("%d", &num);
+//         sum += num;
+//         count++;
+//     }
+
+//     average = sum / 10.0;
+//     printf("The sum of 10 numbers is %d\n", sum);
+//     printf("The average is %.2lf\n", average);
+//     return 0;
+// }
+
 #include <stdio.h>
 
-int main(){
+int main() {
     int sum = 0;
     int num;
     double average;
-    printf("Please enter 10 numbers:\n");
-    for (int i = 1; i <= 10; i++){
-        printf("Number #%d>> ", i);
+    int count = 0;
+    do {
+        printf("Please enter number #%d>> ", count + 1);
         scanf("%d", &num);
         sum += num;
-    }
+        count++;
+    } while (count < 10);
 
     average = sum / 10.0;
     printf("The sum of 10 numbers is %d\n", sum);
     printf("The average is %.2lf\n", average);
     return 0;
 }
-
